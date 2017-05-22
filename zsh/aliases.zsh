6ed2cbc5d52dba4fba0496ebf6802dbbe83d2278
Adrian Vasile <adivasile07@gmail.com>
2017-05-22 14:16:48 +0300

    changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,5 @@
 alias reload!='. ~/.zshrc'
+alias hstart="/usr/local/Cellar/hadoop/2.7.3/sbin/start-dfs.sh;/usr/local/Cellar/hadoop/2.7.3/sbin/start-yarn.sh"
+alias hstop="/usr/local/Cellar/hadoop/2.7.3/sbin/stop-yarn.sh;/usr/local/Cellar/hadoop/2.7.3/sbin/stop-dfs.sh"
 alias vim=nvim
+alias sshconf='vim ~/.ssh/config'
