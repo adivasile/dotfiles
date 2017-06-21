b660c704936196b24e87e440f45bb0c3bc7303d0
Adrian Vasile <adivasile07@gmail.com>
2017-06-21 07:22:51 +0300

    cleanup and improvements

---
+++ hadoop/path.zsh
@@ -0,0 +1,5 @@
+export HIVE_HOME=/Users/adrianvasile/apache-hive-2.1.0-bin
+export HADOOP_HOME=/Users/adrianvasile/hadoop-2.7.3
+
+export PATH="$PATH:$HIVE_HOME/bin" # Add hive binaries to path
+export PATH="$PATH:$HADOOP_HOME/bin" # Add hive binaries to path
