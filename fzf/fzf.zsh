a8bbb786dfa56aec6d0722ad276c55db57423cb6
Adrian Vasile <adivasile07@gmail.com>
2019-02-22 10:31:46 +0200

    tmux clipboard integration; vim config cleanup; switch to rg

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,2 +1,2 @@
 export FZF_DEFAULT_OPTS="--border --inline-info --height 40%"
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
