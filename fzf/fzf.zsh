ce3f2afe4b43a6450b5ced391e74c4a57737b1a3
Adrian Vasile <adivasile07@gmail.com>
2019-04-05 09:52:19 +0300

    fixes vim search

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,2 +1,2 @@
 export FZF_DEFAULT_OPTS="--border --inline-info --height 40%"
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --fixed-strings --no-ignore --hidden --follow --glob "!.git/*,!node_modules/*"'
