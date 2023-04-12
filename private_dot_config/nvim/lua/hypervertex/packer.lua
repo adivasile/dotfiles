1ddf1c705f972aca457f466ede38d5409073aac8
Adrian Vasile <adivasile07@gmail.com>
2023-04-12 15:47:46 +0300

    new vim conf with lua

---
+++ private_dot_config/nvim/lua/hypervertex/packer.lua
@@ -0,0 +1,64 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({ 'rose-pine/neovim', as = 'rose-pine', })
+	use({ 'dracula/vim', as = 'dracula', })
+
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use('nvim-treesitter/playground')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			    'williamboman/mason.nvim',
+			    run = function()
+				    pcall(vim.cmd, 'MasonUpdate')
+			    end,
+		    },
+		    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+	    }
+    }
+
+    use('scrooloose/nerdtree', { on = 'NERDTreeToggle' })
+
+    use('tpope/vim-surround')
+    use('tpope/vim-repeat')
+    use('tpope/vim-commentary')
+    use('itchyny/lightline.vim')
+    use('christoomey/vim-tmux-navigator')
+    use('tmux-plugins/vim-tmux-focus-events')
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+end)
---
+++ private_dot_config/nvim/lua/hypervertex/remap.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+
+-- Integrate system clipboard
+vim.keymap.set("n", "<leader>p", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<leader>cp", ":let @+=@%<CR>")
+vim.keymap.set("n", "<leader>cf", ":let @+=expand(\"%:t:r\")<CR>")
+
+vim.keymap.set("n", "<C-j>", "<C-W>j")
+vim.keymap.set("n", "<C-k>", "<C-W>k")
+vim.keymap.set("n", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-l>", "<C-W>l")
+
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>") 
+vim.keymap.set("n", "<leader>s", ":split<CR>") 
+vim.keymap.set("n", "<leader>x", ":q<CR>") 
+vim.keymap.set("n", "<leader>w", ":w<CR>") 
+
+vim.keymap.set("n", "E", "$")
+vim.keymap.set("n", "B", "^")
+vim.keymap.set("i", "nn", "<ESC>")
+
+vim.keymap.set("n", "Q", "<nop>")
